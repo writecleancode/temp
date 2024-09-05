1d25474dafc4a612eb5ae30c2a7728c10bb0d0e0
Rafal <rafek131@wp.pl>
2024-09-05 19:08:53 +0200

    'isNumberInArrayOfNumbers' function testing

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,19 @@
-const fibonacci = (f) => {
-    const f0 = 0
-    const f1 = 1
-    let x = f0
-    let y = f0 + f1
-    
-      for (let i = 1; i < f; i++) {
-        y += x
-        x = y - x
-      };  
-    
-    console.log(y)
-  }
-  
-  fibonacci(7)
+const arr = [
+	[3, 21, 37],
+	[61, 79, 101, 120],
+	[133, 149],
+];
+
+const isNumberInArrayOfNumbers = (number, array) => {
+	for (let i = 0; i < array.length; i++) {
+		if (number >= array[i][0] && number <= array[i].at(-1)) {
+			return console.log(array[i].includes(number));
+		}
+	}
+
+	return console.log(false);
+};
+
+isNumberInArrayOfNumbers(0, arr); //false
+isNumberInArrayOfNumbers(21, arr); //true
+isNumberInArrayOfNumbers(221, arr); //false
