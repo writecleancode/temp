e9436222c09685bccd1ec80e55cbbc9e39b75574
Rafal <rafek131@wp.pl>
2024-09-05 20:15:46 +0200

    further 'isNumberInArrayOfNumbers' function testing

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,41 @@
-const arr = [
-	[3, 21, 37],
-	[61, 79, 101, 120],
-	[133, 149],
-];
+// const arr = [
+// 	[3, 21, 37],
+// 	[61, 79, 101, 120],
+// 	[133, 149],
+// ];
 
-const isNumberInArrayOfNumbers = (number, array) => {
-	for (let i = 0; i < array.length; i++) {
-		if (number >= array[i][0] && number <= array[i].at(-1)) {
-			return console.log(array[i].includes(number));
+// const isNumberInArrayOfNumbers = (number, array) => {
+// 	for (let i = 0; i < array.length; i++) {
+// 		if (number >= array[i][0] && number <= array[i].at(-1)) {
+// 			return console.log(array[i].includes(number));
+// 		}
+// 	}
+
+// 	return console.log(false);
+// };
+
+// isNumberInArrayOfNumbers(0, arr); //false
+// isNumberInArrayOfNumbers(21, arr); //true
+// isNumberInArrayOfNumbers(221, arr); //false
+
+const arr = [1, 2, 3];
+
+const findNumber = (number, array) => {
+	if (array.length === 1) {
+		if (array[0] === number) {
+			return console.log(true);
+		} else {
+			return console.log(false);
 		}
 	}
 
-	return console.log(false);
+	if (array[Math.ceil((array.length - 1) / 2)] === number) {
+		return console.log(true);
+	} else if (array[Math.ceil((array.length - 1) / 2)] > number) {
+		findNumber(number, array.slice(0, (array.length - 1) / 2));
+	} else if (array[Math.ceil((array.length - 1) / 2)] < number) {
+		findNumber(number, array.slice((array.length - 1) / 2 + 1));
+	}
 };
 
-isNumberInArrayOfNumbers(0, arr); //false
-isNumberInArrayOfNumbers(21, arr); //true
-isNumberInArrayOfNumbers(221, arr); //false
+findNumber(2, arr);
