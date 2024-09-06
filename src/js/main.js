ddb9a9e674abb8226a9b6657d94244a13e468354
Rafal <rafek131@wp.pl>
2024-09-06 12:28:55 +0200

    completed 'isNumberInArrayOfNumbers' function

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,8 @@
-// const arr = [
-// 	[3, 21, 37],
-// 	[61, 79, 101, 120],
-// 	[133, 149],
-// ];
-
-// const isNumberInArrayOfNumbers = (number, array) => {
-// 	for (let i = 0; i < array.length; i++) {
-// 		if (number >= array[i][0] && number <= array[i].at(-1)) {
-// 			return console.log(array[i].includes(number));
-// 		}
-// 	}
-
-// 	return console.log(false);
-// };
-
-// isNumberInArrayOfNumbers(0, arr); //false
-// isNumberInArrayOfNumbers(21, arr); //true
-// isNumberInArrayOfNumbers(221, arr); //false
-
-const arr = [12, 20, 36, 45, 51, 70];
+const arr = [
+	[3, 21, 37],
+	[61, 79, 101, 120],
+	[133, 149],
+];
 
 const findNumber = (number, array) => {
 	if (array.length === 1) {
@@ -41,4 +25,16 @@ const findNumber = (number, array) => {
 	}
 };
 
-findNumber(20, arr);
+const isNumberInArrayOfNumbers = (number, array) => {
+	for (let i = 0; i < array.length; i++) {
+		if (number >= array[i][0] && number <= array[i].at(-1)) {
+			return findNumber(number, array[i]);
+		}
+	}
+
+	return console.log(false);
+};
+
+isNumberInArrayOfNumbers(0, arr); //false
+isNumberInArrayOfNumbers(21, arr); //true
+isNumberInArrayOfNumbers(221, arr); //false
