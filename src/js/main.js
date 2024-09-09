6077183a1d04ef2d383ce1c7b4deebb446960f78
Rafal <rafek131@wp.pl>
2024-09-09 17:06:22 +0200

    inheritance testing - classes

--- src/js/main.js
+++ src/js/main.js
@@ -24,3 +24,14 @@ const car4 = new Vehicle();
 
 car3.drive();
 car4.drive();
+
+// =====================================
+
+car3.brand = 'Volkswagen';
+console.log(car3);
+
+console.log(Vehicle);
+console.log(Vehicle.prototype)
+console.log(Vehicle.prototype === car3.__proto__)
+
+console.log(car3.__proto__.drive)
