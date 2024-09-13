a72be99e5e290ecba525c2b1c14fda2132dc09e3
Rafal <rafek131@wp.pl>
2024-09-13 15:51:44 +0200

    event loop tests

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,13 @@
 console.log("start");
 
 // microtask
+queueMicrotask(() => {
+   console.log("queueMicrotask");
+});
+
+// microtask
 Promise.resolve().then(() => {
-    console.log("Promise");
-  });
+   console.log("Promise");
+});
 
 console.log("end");
