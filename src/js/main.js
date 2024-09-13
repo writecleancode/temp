ba798a722e4e049716f4ab8aca6669feec7c4c26
Rafal <rafek131@wp.pl>
2024-09-13 15:27:03 +0200

    event loop tests

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,20 @@
-if (true) {
-	var varVariable = 'This is true';
-}
-
-console.log(varVariable);
-
-if (true) {
-	let letVariable = 'This is true';
-}
-
-// console.log(letVariable);		// throws error
-
-// =====================================
-console.log('---------------------');
-
-var varVariable = 'False!';
-console.log(varVariable);
-
-// =====================================
-console.log('---------------------');
-
-console.log(anotherVarVariable); // returs undefined insted of throwing error
-var anotherVarVariable = 'test message';
+function rock() {
+	console.log("rock");
+ }
+ 
+ function paper() {
+	console.log("paper");
+ }
+ 
+ function scissor() {
+	console.log("scissor");
+ }
+ 
+ function play() {
+	rock();
+	paper();
+	scissor();
+ }
+ 
+ console.clear();
+ play();
