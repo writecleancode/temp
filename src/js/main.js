2d44c07c1a2e07227ce2d6b17c778e35e9972c99
Rafal <rafek131@wp.pl>
2024-09-13 15:43:48 +0200

    event loop tests

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,5 @@
-function inquireCriminalRecords(cutomerSSN) {
-	// check with an external API
-	// It will return the number of criminal records that found for this Social Serial Number
-	console.log("Calling External API for checking criminal records for SSN: " + cutomerSSN);
-	return 0;
- }
- 
- function checkStatus(cutomerSSN) {
-	console.log("Checking status started for: " + cutomerSSN);
-	let checkResult = inquireCriminalRecords(cutomerSSN);
-	if (checkResult === 0) {
-	   console.log("checkStatus was Positive");
-	   return "Positive";
-	} else {
-	   console.log("There was a problem with checkStatus");
-	   return "Negative";
-	}
- }
- 
- function openBankAccount(cutomerSSN) {
-	let checkResult = checkStatus(cutomerSSN);
-	if (checkResult === "Positive") {
-	   // Opens the Account
-	   console.log("Bank Account created successfully");
-	} else {
-	   console.log("There was a problem On customer check. contact security department for more details");
-	}
- }
- 
- console.clear();
- openBankAccount(6525865214);
+console.log("1");
+
+console.log("2");
+
+console.log("3");
