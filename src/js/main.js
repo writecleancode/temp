a14cb52f4b3192ab90cdaad5bc5b967a1191ffc8
Rafal <rafek131@wp.pl>
2024-09-20 14:40:46 +0200

    prototypal inheritance

--- src/js/main.js
+++ src/js/main.js
@@ -1,93 +1,18 @@
-class Monster {
-	constructor(name) {
-		this.name = name;
-	}
-
-	attack() {
-		console.log(`${this.name} attacked`);
-	}
-
-	walk() {
-		console.log(`${this.name} walked`);
-	}
+const human = {
+	kind: 'Human'
 }
 
-class FlyingMonster extends Monster {
-	fly() {
-		console.log(`${this.name} flew`);
-	}
-}
+const sina = Object.create(human)
+sina.age = 34
 
-class SwimmingMonster extends Monster {
-	swim() {
-		console.log(`${this.name} swam`);
-	}
-}
+console.log(sina);
+console.log(sina.age);
+console.log(sina.kind);
 
-const bear = new Monster('bear');
-bear.walk();
-bear.attack();
+const ben = Object.create(sina)
+ben.age = 12
 
-const eagle = new FlyingMonster('eagle');
-eagle.walk();
-eagle.attack();
-eagle.fly();
-
-const shark = new SwimmingMonster('shark');
-shark.walk();
-shark.attack();
-shark.swim();
-
-// =====================================
-console.log('---------------');
-
-function attackerAndWalker({ name }) {
-	return {
-		attack: () => console.log(`${name} attacked`),
-		walk: () => console.log(`${name} walked`),
-	};
-}
-
-function swimmer({ name }) {
-	return {
-		swim: () => console.log(`${name} swam`),
-	};
-}
-
-function flyer({ name }) {
-	return {
-		fly: () => console.log(`${name} flew`),
-	};
-}
-
-function swimmingMonsterCreator(name) {
-	const monster = { name: name };
-
-	return {
-		...monster,
-		...attackerAndWalker(monster),
-		...swimmer(monster),
-	};
-}
-
-function flyingSwimmingMonsterCreator(name) {
-	const monster = { name: name };
-
-	return {
-		...monster,
-		...attackerAndWalker(monster),
-		...swimmer(monster),
-		...flyer(monster),
-	};
-}
-
-const dolphin = swimmingMonsterCreator('dolphin');
-dolphin.walk();
-dolphin.attack();
-dolphin.swim();
-
-const duck = flyingSwimmingMonsterCreator('duck');
-duck.walk();
-duck.attack();
-duck.swim();
-duck.fly();
+console.log(ben);
+console.log(ben.age);
+console.log(ben.__proto__.age);
+console.log(ben.kind);
