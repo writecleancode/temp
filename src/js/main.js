b25396e6d556a3989be7e94885243da42e9470d0
Rafal <rafek131@wp.pl>
2024-09-25 11:07:53 +0200

    value & reference test

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,10 @@ const testVal2 = 'volkswagen';
 
 console.log(testVal1 === testVal2);
 console.log(testVal1.toLowerCase() === testVal2);
+
+const testFunc1 = () => {};
+const testFunc2 = () => {};
+const testFunc3 = testFunc1;
+
+console.log(testFunc1 === testFunc2);
+console.log(testFunc1 === testFunc3);
