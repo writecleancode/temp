96f8c6c7db11150818f132ea9eeb05272efd3a37
Rafal <rafek131@wp.pl>
2024-09-30 15:41:19 +0200

    'this' test

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,10 @@
-const testVal1 = 'Volkswagen';
-const testVal2 = 'volkswagen';
+const testObject = {
+	brand: 'Volkswagen',
+	testFunction() {
+		(() => {
+			console.log(this);
+		})();
+	},
+};
 
-console.log(testVal1 === testVal2);
-console.log(testVal1.toLowerCase() === testVal2);
-
-const testFunc1 = () => {};
-const testFunc2 = () => {};
-const testFunc3 = testFunc1;
-
-console.log(testFunc1 === testFunc2);
-console.log(testFunc1 === testFunc3);
+testObject.testFunction();
