e7f2a92f331d2e9e19b03716e8005e8894bc64df
Rafal <rafek131@wp.pl>
2024-09-30 17:24:17 +0200

    simple weather app

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,17 @@
-const testObject = {
-	brand: 'Volkswagen',
-	testFunction() {
-		(() => {
-			console.log(this);
-		})();
-	},
+const fetchData = () => {
+	return new Promise((resolve, reject) => {
+		fetch('https://api.weather.gov/gridpoints/OKX/35,35/forecast')
+			.then(res => res.json())
+			.then(data => resolve(data.properties.periods[1].shortForecast));
+	});
 };
 
-testObject.testFunction();
+const displayWeather = weather => {
+	console.log(weather);
+};
+
+const onError = error => {
+	console.log(`Error: ${error}`);
+};
+
+fetchData().then(displayWeather).catch(onError);
