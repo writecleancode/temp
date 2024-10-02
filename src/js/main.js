78b830c9a8d3d0261c65d2eacc0d80baafe6468e
Rafal <rafek131@wp.pl>
2024-10-02 15:15:25 +0200

    3 different API handlers

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,36 @@
-const fetchData = () => {
-	return new Promise((resolve, reject) => {
-		fetch('https://api.weather.gov/gridpoints/OKX/35,35/forecast')
-			.then(res => res.json())
-			.then(data => resolve(data.properties.periods[1].shortForecast));
-	});
+const xhr = new XMLHttpRequest();
+
+function getCars() {
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState == XMLHttpRequest.DONE) {
+			const response = xhr.responseText;
+			const data = JSON.parse(response);
+			console.log(data);
+		}
+	};
+	xhr.open('GET', 'https://freetestapi.com/api/v1/cars', true);
+	xhr.send(null);
+}
+
+getCars();
+
+// ----------------------------------------
+
+fetch('https://freetestapi.com/api/v1/cars')
+	.then(res => res.json())
+	.then(data => console.log(data))
+	.catch(error => console.log(error));
+
+// ----------------------------------------
+
+const getCarsAsync = async () => {
+	try {
+		const res = await fetch('https://freetestapi.com/api/v1/cars');
+		const data = await res.json();
+		console.log(data);
+	} catch (error) {
+		console.log(error);
+	}
 };
 
-const displayWeather = weather => {
-	console.log(weather);
-};
-
-const onError = error => {
-	console.log(`Error: ${error}`);
-};
-
-fetchData().then(displayWeather).catch(onError);
+getCarsAsync();
